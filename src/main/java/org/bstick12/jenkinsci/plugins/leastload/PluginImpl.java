fa2033cc5eeef9ff67e795fb93a0ac3bec4e9e73
brendan.nolan <brendan.nolan@gmail.com>
2013-05-28 13:44:00 +0100

    Preparing for hosting request

--- src/main/java/org/bstick12/jenkinsci/plugins/leastload/PluginImpl.java
+++ src/main/java/org/bstick12/jenkinsci/plugins/leastload/PluginImpl.java
@@ -1,18 +1,19 @@
 package org.bstick12.jenkinsci.plugins.leastload;
 
-import jenkins.model.Jenkins;
 import hudson.Plugin;
 import hudson.model.LoadBalancer;
 
+import java.util.logging.Logger;
+
+import jenkins.model.Jenkins;
+
 public class PluginImpl extends Plugin {
-	
+		
 	@Override
 	public void start() throws Exception {
-
 		LoadBalancer currentLoadBalancer = Jenkins.getInstance().getQueue().getLoadBalancer();
 		LoadBalancer leastLoadBalancer = new LeastLoadBalancer(currentLoadBalancer);
 		Jenkins.getInstance().getQueue().setLoadBalancer(leastLoadBalancer);
-
 	}
 	
 }
